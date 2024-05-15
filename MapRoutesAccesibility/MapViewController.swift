523c413b40d189866af1338463cabe769dd494e7
emolio90 <emolio90@gmail.com>
2024-05-16 01:36:36 +0200

    Add reverseGeocoding on map tap

--- MapRoutesAccesibility/MapViewController.swift
+++ MapRoutesAccesibility/MapViewController.swift
@@ -13,6 +13,7 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     private var pois: [PointOfInterest] = []
+    private var temporaryPin: MKPointAnnotation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +35,65 @@ class MapViewController: UIViewController {
 //        mapView.addAnnotation(aceituna)
         loadInitialData()
         mapView.addAnnotations(pois)
+        
+        
+        
+        // Add a gesture recognizer to detect taps on the map
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        mapView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
+        if gestureRecognizer.state == .ended {
+            let tapPoint = gestureRecognizer.location(in: mapView)
+            let coordinate = mapView.convert(tapPoint, toCoordinateFrom: mapView)
+            print("Tapped at coordinate: \(coordinate.latitude), \(coordinate.longitude)")
+            
+            addTemporaryPin(at: coordinate)
+            
+            reverseGeocoding(coordinate)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.removeTemporaryPin()
+            }
+        }
+    }
+    
+    private func addTemporaryPin(at coordinate: CLLocationCoordinate2D) {
+        removeTemporaryPin()
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+        temporaryPin = annotation
+        
+    }
+    
+    private func removeTemporaryPin() {
+        if let pin = temporaryPin {
+            mapView.removeAnnotation(pin)
+            temporaryPin = nil
+        }
+    }
+    
+    private func reverseGeocoding(_ coordinate: CLLocationCoordinate2D) {
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print("Reverse geocoding error: \(error.localizedDescription)")
+            }
+            guard let placemark = placemarks?.first else {
+                print("No placemark found")
+                return
+            }
+            
+            if let formattedAddress = placemark.formattedAddress {
+                print("Address: \(formattedAddress)")
+            } else {
+                print("No address found")
+            }
+        }
     }
     
     private func loadInitialData() {
@@ -72,22 +132,27 @@ private extension MKMapView {
 
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
-        guard let annotation = annotation as? PointOfInterest else {
-            return nil
+        if let annotation = annotation as? PointOfInterest {
+            let identifier = "cat"
+            var view: MKMarkerAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            }
+            return view
+        } else if annotation === temporaryPin {
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            pinView.pinTintColor = .gray
+            pinView.animatesDrop = true
+            return pinView
         }
         
-        let identifier = "cat"
-        var view: MKMarkerAnnotationView
-        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
-            dequeuedView.annotation = annotation
-            view = dequeuedView
-        } else {
-            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            view.canShowCallout = true
-            view.calloutOffset = CGPoint(x: -5, y: 5)
-            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        return view
+        return nil
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
@@ -97,5 +162,13 @@ extension MapViewController: MKMapViewDelegate {
         
         let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
         pointOfInterest.mapItem?.openInMaps(launchOptions: launchOptions)
+    }
+}
+
+
+extension CLPlacemark {
+    var formattedAddress: String? {
+        guard let name = name, let locality = locality, let country = country else { return nil }
+        return "\(name), \(locality), \(country)"
     }
 }
